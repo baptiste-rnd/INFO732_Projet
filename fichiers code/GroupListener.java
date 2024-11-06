2ffeb9d83bb817ce4f65cc57e6c4a1496d308c87
skiiing73 <corentindegouey@gmail.com>
2024-11-06 17:14:37 +0100

    resolution bug

--- fichiers code/GroupListener.java
+++
@@ -1,9 +0,0 @@
-package fichiers_code;
-
-public interface GroupListener {
-    void onMessageRemoved(Group group, Message message);
-    void onDocumentRemoved(Group group, Document document);
-    void onUserInvited(Group group, User user);
-    void onUserExcluded(Group group, User user);
-    void onMessageReceived(Group group);  // Méthode pour notifier la réception d'un message
-}
